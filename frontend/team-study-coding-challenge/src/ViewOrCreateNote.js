a15d95055d6bdcaf50b036ff7601d3087e31159e
Varun <varunbatta@hotmail.com>
2019-12-01 21:11:10 -0500

    Correcting prev git commit

---
+++ frontend/team-study-coding-challenge/src/ViewOrCreateNote.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Quill from 'quill';
+
+class ViewOrCreateNote extends React.Component {        
+    constructor(props) {
+        super(props);
+        this.saveNote = this.saveNote.bind(this);
+    }
+    
+    componentDidMount() {
+        const config = {
+            theme: 'snow'
+        };
+        const titleQuill = new Quill(".note-title", config);
+        const bodyQuill = new Quill(".note-body", config);
+
+        const noteElem = this.props.noteElem;
+        const titleQuillDelta = {
+            "ops": []
+        };
+        if (noteElem) {
+            titleQuillDelta["ops"].push({"insert": noteElem["title"]});
+        }
+        const bodyQuillDelta = {
+            "ops": []
+        };
+        if (noteElem) {
+            bodyQuillDelta["ops"].push({"insert": noteElem["body"]});
+        }
+
+        titleQuill.setContents(titleQuillDelta);
+        titleQuill.enable(!!!noteElem);
+        bodyQuill.setContents(bodyQuillDelta);
+        bodyQuill.enable(!!!noteElem);
+    }
+
+    saveNote() {
+        const titleQuill = new Quill(".note-title");
+        const bodyQuill = new Quill(".note-body");
+
+        const titleData = titleQuill.getContents().ops[0]["insert"]
+        const bodyData = bodyQuill.getContents().ops[0]["insert"]
+        this.props.saveNote(titleData, bodyData);
+        this.props.unselectNote();
+    }
+    
+    render() {
+        return (
+            <div>
+                <div>Title:</div>
+                <div className="note-title"></div>
+                <div>Body:</div>
+                <div className="note-body"></div>
+                <div onClick={!!this.props.noteElem ? this.props.unselectNote : this.saveNote}>{!!this.props.noteElem ? "View All Notes" : "Save Note"}</div>
+            </div>
+        )
+    }
+}
+
+export default ViewOrCreateNote;
---
+++ frontend/team-study-coding-challenge/src/index.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import ViewOrCreateNote from "./ViewOrCreateNote.js";
+
+class ListOfNotes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: [],
+            viewOrEditNote: false,
+            selectedNoteIndex: -1
+        }
+        this.unselectNote = this.unselectNote.bind(this);
+    }
+
+    async getAllNotes() {
+        await fetch("http://localhost:8000/notes/")
+            .then(res => res.text())
+            .then(res => this.setState(
+                JSON.parse(res)
+            ));
+    }
+
+    componentWillMount() {
+        this.getAllNotes();
+    }
+
+    selectNote(noteIndex) {
+        this.setState({
+            notes: this.state.notes,
+            viewOrEditNote: true,
+            selectedNoteIndex: noteIndex
+        });
+    }
+
+    unselectNote() {
+        this.setState({
+            notes: this.state.notes,
+            viewOrEditNote: false,
+            selectedNoteIndex: -1
+        });
+        this.getAllNotes();
+    }
+
+    async saveNote(noteTitle, noteBody) {
+        await fetch("http://localhost:8000/notes/create-note/", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                title: noteTitle,
+                body: noteBody
+            })
+        });
+    }
+    
+    render() {
+        return this.state.viewOrEditNote ? 
+        (
+            <ViewOrCreateNote
+                noteElem={this.state.notes[this.state.selectedNoteIndex]}
+                unselectNote={this.unselectNote}
+                saveNote={this.saveNote}
+            />
+        )
+        : 
+        (
+            <div className="notes-list">
+                <h1>Available notes: </h1>
+                <ul>
+                    {this.state.notes.map((element, index) => 
+                        <div key={index} onClick={(e) => this.selectNote(index)}>{element.title}</div>
+                    )}
+                </ul>
+                <h2 onClick={(index) => this.selectNote(-1)}>+ New Note</h2>
+            </div>
+        )
+    }
+}
+
+// =============================
+
+ReactDOM.render(<ListOfNotes />, document.getElementById('root'));
